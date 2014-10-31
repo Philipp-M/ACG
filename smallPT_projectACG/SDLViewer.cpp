25e9f5211ef866b0ced1fcba68cf50c1f0142c48
Philipp Mildenberger <philipp.mildenberger@koeln.de>
2014-10-31 21:49:50 +0100

    SDLViewer class added

---
+++ smallPT_projectACG/SDLViewer.cpp
@@ -0,0 +1,13 @@
+#include "SDLViewer.hpp"
+
+SDLViewer::SDLViewer()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+SDLViewer::~SDLViewer()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ smallPT_projectACG/SDLViewer.hpp
@@ -0,0 +1,11 @@
+#ifndef SDLVIEWER_HPP_
+#define SDLVIEWER_HPP_
+
+class SDLViewer
+{
+public:
+	SDLViewer();
+	virtual ~SDLViewer();
+};
+
+#endif /* SDLVIEWER_HPP_ */
