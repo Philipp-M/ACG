2c44c279df1e521f7b1896f74e2f72a122c31e97
Philipp Mildenberger <philipp.mildenberger@koeln.de>
2014-11-01 03:17:33 +0100

    completed the SDL Viewer class, now it has a nice 'realtime' view, with a save image function

--- smallPT_projectACG/main.cpp
+++ smallPT_projectACG/main.cpp
@@ -4,76 +4,21 @@
 #include "GTriangle.hpp"
 #include "GPolygonObject.hpp"
 #include "ObjLoader.hpp"
+#include "SDLViewer.hpp"
 #include <cstdlib>
 #include <cstdio>
 
 int main(int argc, char *argv[])
 {
 	int w = 512, h = 512; // image size
-	int samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples (default of 1
-	//Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm()); // camera pos, dir
-	Ray cam(Vec(50, 50, 230), Vec(0, 0, -1).norm()); // camera pos, dir
-	Vec cx = Vec(w * .7135 / h); // x direction increment (uses implicit 0 for y, z)
-	Vec cy = (cx % cam.direction).norm() * .7135; // y direction increment (note cross product)
-	Vec r; // used for colors of samples
-	Vec *c = new Vec[w * h]; // The image
-
-	/********** create the scene *************/
-	GScene scene;
-//	scene.addItem(new GSphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75), DIFF));
-//	scene.addItem(new GSphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC));
-//	scene.addItem(new GSphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFR));
-//	scene.addItem(new GSphere(1.5, Vec(50, 81.6 - 16.5, 81.6), Vec(4, 4, 4) * 100, Vec(), DIFF));
-	std::vector<GPolygonObject*> sceneObj;
-	sceneObj = ObjLoader::loadOfFile("scenes/cornell.obj");
-	for(size_t i = 0; i < sceneObj.size(); i++)
-		scene.addItem(sceneObj[i]);
-//	scene.addItem(new GSphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF));
-//	scene.addItem(new GSphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75), DIFF));
-//	scene.addItem(new GSphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC));
-//	scene.addItem(new GTriangle(Vec(17, 34, 20), Vec(27+20, 60+20, 20-10), Vec(27-20, 60-5, 20+30), Vec(), Vec(0.5, 1, 0) * .999, DIFF));
-//	scene.addItem(new GSphere(16.5, Vec(85, 16.5, 78), Vec(), Vec(0.9, 0.8, 0.8) * .999, REFR));
-//	scene.addItem(new GSphere(600, Vec(50, 681.6 - .27, 81.6), Vec(12, 12, 12), Vec(), DIFF));
-
-#pragma omp parallel for schedule(dynamic, 1) private(r) // OpenMP
-
-	// LOOP OVER ALL IMAGE PIXELS
-	for (int y = 0; y < h; y++)
-	{ // Loop over image rows
-		fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1)); // print progress
-		for (unsigned short x = 0, Xi[3] =
-		{ 0, 0, y * y * y }; x < w; x++)   // Loop columns
-
-			// FOR EACH PIXEL DO 2x2 SUBSAMPLES, AND samps SAMPLES PER SUBSAMPLE
-			for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++)     // 2x2 subpixel rows
-				for (int sx = 0; sx < 2; sx++, r = Vec())
-				{        // 2x2 subpixel cols
-					for (int s = 0; s < samps; s++)
-					{
-						// I BELIEVE THIS PRODUCES A TENT FILTER
-						double r1 = 2 * erand48(Xi), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-						double r2 = 2 * erand48(Xi), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
-						Vec d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) + cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.direction;
-						r = r + SmallPT::radiance(Ray(cam.origin + d * 140, d.norm()), &scene, 0, Xi) * (1. / samps);
-					} // Camera rays are pushed ^^^^^ forward to start in interior
-					c[i] = c[i] + Vec(SmallPT::clamp(r.x), SmallPT::clamp(r.y), SmallPT::clamp(r.z)) * .25;
-				}
-	}
-
-	// WRITE OUT THE FILE TO A PPM
-	FILE *f = fopen("image.ppm", "w");         // Write image to PPM file.
-	fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
-	for (int i = 0; i < w * h; i++)
+	int samplePerStep = 4;
+	if(argc > 2)
 	{
-		fprintf(f, "%d %d %d ", SmallPT::toInt(c[i].x), SmallPT::toInt(c[i].y), SmallPT::toInt(c[i].z));
+		w = std::stoi(argv[1]);
+		h = std::stoi(argv[2]);
 	}
+	if(argc > 3)
+		samplePerStep = std::stoi(argv[3]);
+	SDLViewer view(w,h, samplePerStep);
+	view.display();
 }
