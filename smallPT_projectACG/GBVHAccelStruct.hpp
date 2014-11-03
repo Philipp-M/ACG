8ab1c4d6f4baac063f9578e266aa3667aa5f726d
Philipp Mildenberger <philipp.mildenberger@koeln.de>
2014-11-04 00:49:00 +0100

    Bounding Box 'completed'

--- smallPT_projectACG/GBVHAccelStruct.hpp
+++ smallPT_projectACG/GBVHAccelStruct.hpp
@@ -2,7 +2,7 @@
 #define GBVHACCELSTRUCT_HPP_
 
 #include "DataTypes.hpp"
-
+#include <algorithm>
 class GBoundingVolume
 {
 public:
@@ -10,11 +10,36 @@ public:
 	virtual ~GBoundingVolume();
 };
 
-class GBoundingBox : GBoundingVolume
+class GBoundingBox: GBoundingVolume
 {
 public:
-	bool intersect(const Ray& ray) const;
-
+	GBoundingBox(const Vec& min, const Vec& max) : min(min), max(max) {}
+	bool intersect(const Ray& ray) const
+	{
+		double tmin = (min.x - ray.origin.x) / ray.direction.x;
+		double tmax = (max.x - ray.origin.x) / ray.direction.x;
+		if (tmin > tmax)
+			std::swap(tmin, tmax);
+		double tymin = (min.y - ray.origin.y) / ray.direction.y;
+		double tymax = (max.y - ray.origin.y) / ray.direction.y;
+		if (tymin > tymax)
+			std::swap(tymin, tymax);
+		if ((tmin > tymax) || (tymin > tmax))
+			return false;
+		if (tymin > tmin)
+			tmin = tymin;
+		if (tymax < tmax)
+			tmax = tymax;
+		double tzmin = (min.z - ray.origin.z) / ray.direction.z;
+		double tzmax = (max.z - ray.origin.z) / ray.direction.z;
+		if (tzmin > tzmax)
+			std::swap(tzmin, tzmax);
+		if ((tmin > tzmax) || (tzmin > tmax))
+			return false;
+		return true;
+	}
+private:
+	Vec min, max; // 2 coordinates are enough for describing a Bounding Box
 };
 
 class GBVHAccelStruct
