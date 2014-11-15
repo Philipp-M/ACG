a5b3f49a993b2b99c7848f3e14bc59b81e56932d
Philipp Mildenberger <philipp.mildenberger@koeln.de>
2014-11-15 14:44:24 +0100

    added TextureManager

---
+++ smallPT_projectACG/TextureManager.hpp
@@ -0,0 +1,39 @@
+#ifndef TEXTUREMANAGER_HPP_
+#define TEXTUREMANAGER_HPP_
+#include <vector>
+#include <string>
+
+struct Texture
+{
+	std::string name; // maybe not needed
+	size_t width;
+	size_t height;
+	uint8_t *pixels;
+};
+
+class TextureManager
+{
+public:
+	virtual ~TextureManager()
+	{
+		for( const Texture& tex : textures)
+			delete[] tex.pixels;
+	}
+	/*** Singleton Instance ***/
+	static TextureManager& get()
+	{
+		static TextureManager instance;
+		return instance;
+	}
+	/**
+	 * for now just 'BMP' support
+	 */
+	int loadTexture(const std::string& filename);
+	const Texture* getByID (uint32_t id) { return textures.size() <= id ? NULL : &textures[id]; }
+private:
+	std::vector<Texture> textures;
+	// private constructor to prevent instancing
+	TextureManager() { }
+};
+
+#endif /* TEXTUREMANAGER_HPP_ */
