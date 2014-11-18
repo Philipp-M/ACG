d08831d70d5e5e6a77af3089067e8f5dc3d12f45
Philipp Mildenberger <philipp.mildenberger@koeln.de>
2014-11-18 22:34:40 +0100

    started implementing normal map and reordered the other maps, so that they can exist standalone

--- smallPT_projectACG/GTexturedTriangle.cpp
+++ smallPT_projectACG/GTexturedTriangle.cpp
@@ -54,25 +54,52 @@ bool GTexturedTriangle::intersect(const Ray& ray, RayIntPt& intPoint) const
 	intPoint.emission = emission;
 	intPoint.reflType = refl;
 	intPoint.color = color;
-	if (colorMap != NULL)
+	if (colorMap != NULL || normalMap != NULL || specularMap != NULL)
 	{
 		double w = 1.0 - u - v;
-		Vec2 texCoordinate = (ut*w + vt*u + wt*v).mult(Vec2(colorMap->width, colorMap->height)); // normal and specular map should hold the same width/height
-		size_t pixelIndex = (size_t) (colorMap->width*(colorMap->height-1)-((int)(texCoordinate.y - 0.0001)) * colorMap->width + ((int)(texCoordinate.x-0.0001)))*4;
-		const uint8_t* pixel = &colorMap->pixels[pixelIndex];
-		intPoint.color = color.mult(Vec3(((double)pixel[0]) * 1.0 / 255.0 * 0.999, ((double)pixel[1]) * 1.0 / 255.0 * 0.999, ((double)pixel[2]) * 1.0 / 255.0 * 0.999));
-		// todo if(normalMap != NULL)
+		Vec2 texCoordinate = ut * w + vt * u + wt * v;
+		if (colorMap != NULL)
+		{
+			size_t pixelIndex = (size_t) (colorMap->width * (colorMap->height - 1) - ((int) (texCoordinate.y * colorMap->height - 0.00001)) * colorMap->width
+					+ ((int) (texCoordinate.x * colorMap->width - 0.00001))) * 4;
+			const uint8_t* pixel = &colorMap->pixels[pixelIndex];
+			intPoint.color = color.mult(
+					Vec3(((double) pixel[0]) * 1.0 / 255.0 * 0.999, ((double) pixel[1]) * 1.0 / 255.0 * 0.999, ((double) pixel[2]) * 1.0 / 255.0 * 0.999));
+		}
+		if (normalMap != NULL)
+		{
+			// calculate normal of the map
+			size_t pixelIndex = (size_t) (specularMap->width * (specularMap->height - 1)
+					- ((int) (texCoordinate.y * specularMap->height - 0.00001)) * specularMap->width + ((int) (texCoordinate.x * specularMap->width - 0.00001)))
+					* 4;
+			const uint8_t* pixel = &normalMap->pixels[pixelIndex];
+			Vec3 texNormal = Vec3(1.0 / 128 * pixel[0] - 1, 1.0 / 128 * pixel[1] - 1, 1.0 / 128 * pixel[2] - 1).norm();
+			Vec3 dv01 = v1 - v0;
+			Vec3 dv02 = v2 - v0;
+			Vec2 duv01 = vt - ut;
+			Vec2 duv02 = vt - ut;
+			double r = 1.0 / (duv01.x * duv02.y - duv01.y * duv02.x);
+			Vec3 tan = (dv01 * duv02.y - dv02 * duv01.y) * r;
+			Vec3 btan = (dv02 * duv01.x - dv01 * duv02.x) * r;
+			Vec3 calcNormal;
+			calcNormal.x = tan.dot(texNormal);
+			calcNormal.y = btan.dot(texNormal);
+			calcNormal.z = normal.dot(texNormal);
+			intPoint.normal = texNormal.norm();
+			//std::cerr << "texNormal is: " << texNormal << " normal is: " << normal << " calculated normal is: " << calcNormal << std::endl;
 
+		}
 		if (specularMap != NULL)
 		{
+			size_t pixelIndex = (size_t) (specularMap->width * (specularMap->height - 1)
+					- ((int) (texCoordinate.y * specularMap->height - 0.00001)) * specularMap->width +
+					((int) (texCoordinate.x * specularMap->width - 0.00001)))* 4;
 			intPoint.glossyRoughness = 1.0 - specularMap->pixels[pixelIndex] * 1.0 / 255.0;
 			if (intPoint.glossyRoughness >= 0.999)
 				intPoint.reflType = DIFF;
 			else
 				intPoint.reflType = GLOSS;
 		}
-		else
-			intPoint.reflType = DIFF;
 	}
 	return true;
 }
