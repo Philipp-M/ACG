6606b23b66da2d0876dc1297b301bd79759a7192
Mivey <cem.okulmus@ymail.com>
2015-01-13 22:59:28 +0100

    Eigen used for vector transformations. It's just a header folder, should work with every compiler if include path is linked

--- proceduralTerrainAndTrees_project2/inc/3DTurtle.hpp
+++ proceduralTerrainAndTrees_project2/inc/3DTurtle.hpp
@@ -1,33 +1,31 @@
-#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
 #include <string>
 #include <set>
 #include <stack>
+#include "Eigen/Dense"
 
 using namespace std;
+using namespace Eigen;
 
-typedef OpenMesh::TriMesh_ArrayKernelT<OpenMesh::DefaultTraits> geometry;
+typedef OpenMesh::PolyMesh_ArrayKernelT<> geometry;
+
 
 typedef struct{
-	float x;
-	float y;
-	float z;
-	float azimuth;
-	float attidue;
+	Vector3d pos;	//position in 3D space
+	Vector3d dir;	//direction to draw
+	Vector3d n;		//the normal vector, useful for generating the actual faces
 } position;
 
-typedef struct{
-	float r;
-	float g;
-	float b;
-} color;
+
 
 class _3DTurtle {
 private:
-	set<color> colors;
+	set<Vector3d> colors;
 	int color_index;
 	float line_width;
 	position pos;		
 	stack<position> state_stack;
+	geometry mesh;
 	
 	//move foward
 	void move(float s);
@@ -41,11 +39,7 @@ private:
 	void reduce_line_width();
 	void increase_color_index();
 public:
-	_3DTurtle() { 
-		pos = { 0.0, 0.0, 0.0, 0.0, 0.0 }; 
-		line_width = 1.0f;
-		color_index = 0;
-	}
+	_3DTurtle();
 
 
 
